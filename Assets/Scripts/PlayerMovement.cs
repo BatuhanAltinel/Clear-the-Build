dfb8af6c22d35bfbe1405872012ca5aee7258565
DESKTOP-UFGH3NC\ELİF <batuhan.altinel@hotmail.com>
2023-01-03 15:55:41 +0300

    Cinemachine , mats,prefabs added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    Animator anim;
     [SerializeField]float moveSpeed = 3f;
     [SerializeField] float jumpPower = 5f;
+    [SerializeField] float turnSpeed = 15f;
+    [SerializeField] Transform[] groundCheckPoints;
+    
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();   
     }
 
@@ -17,22 +22,58 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         MoveInputs();
+        print(IsOnGround());
     }
 
     void MoveInputs()
     {
+        if(Input.GetKey("space") && IsOnGround())
+        {
+            rb.velocity = new Vector3(rb.velocity.x ,Mathf.Clamp((jumpPower * 100) * Time.deltaTime,0f,15f),0);
+            anim.SetBool("IsWalk",false);
+            anim.SetBool("IsJump",true);
+            StartCoroutine(StopJumpAnimation());
+        }
         if(Input.GetKey("a"))
         {
-            rb.velocity = new Vector3((-moveSpeed *100)* Time.deltaTime,rb.velocity.y ,0);
+            rb.velocity = new Vector3(Mathf.Clamp((-moveSpeed *100)* Time.deltaTime,-15f,0f),rb.velocity.y ,0);
+            // transform.rotation = Quaternion.Euler(0f,-90f,0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(0,-90.1f,0),turnSpeed*Time.deltaTime);
+            anim.SetBool("IsWalk",true);
         }
         else if(Input.GetKey("d"))
         {
-            rb.velocity = new Vector3((moveSpeed * 100) * Time.deltaTime,rb.velocity.y ,0);
+            rb.velocity = new Vector3(Mathf.Clamp((moveSpeed * 100) * Time.deltaTime,0f,15f),rb.velocity.y ,0);
+            // transform.rotation = Quaternion.Euler(0f,90f,0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.Euler(0,90.1f,0),turnSpeed*Time.deltaTime);
+            anim.SetBool("IsWalk",true);
         }
-
-        if(Input.GetKey("space"))
+        else
         {
-            rb.velocity = new Vector3(rb.velocity.x ,(jumpPower * 100) * Time.deltaTime,0);
+            anim.SetBool("IsWalk",false);
+            rb.velocity = new Vector3(0f,rb.velocity.y,0f);
         }
+        
+    }
+
+    bool IsOnGround()
+    {
+        bool hit = false;
+        for (int i = 0; i < groundCheckPoints.Length; i++)
+        {
+            hit = Physics.Raycast(groundCheckPoints[i].position,-groundCheckPoints[i].transform.up,0.5f);
+            Debug.DrawRay(groundCheckPoints[i].position,-groundCheckPoints[i].transform.up*0.5f,Color.red);
+        }
+        if(hit)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator StopJumpAnimation()
+    {
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("IsJump",false);
     }
 }
